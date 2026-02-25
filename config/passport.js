f7dfd6c8fbe494dfac4f524d2b403e82c27e74e1
Ojaswi <ojaswideep2020@gmail.com>
2026-02-25 19:49:36 +0530

    added google login

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,8 @@ const jwt = require('jsonwebtoken');
 // configure google strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/api/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
